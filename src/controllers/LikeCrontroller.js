5bca5d0e3027422363b933f06ce02fdb66f55f29
SerginhoLeal <48488987+SerginhoLeal@users.noreply.github.com>
2020-04-28 21:05:05 -0300

    api-like

--- src/controllers/LikeCrontroller.js
+++ src/controllers/LikeCrontroller.js
@@ -7,19 +7,16 @@ module.exports = {
     async store(req, res){
         const { user } = req.headers;//pega o usuario logado.
         const { devId } = req.params;//pega o usuario selecionado e não logado.
-                                     // será que posso trocar params por body? {devId} -> {graficos}
 
         const loggedDev = await Grap.findById(user);//não preciso mexer em nada aqui.
         const targetDev = await Grap2.findById(devId);//eu poderia trocar o findById por create.
-        
-        if(!targetDev){//caso o destinatario não exista.
-            return res.status(400).json({error: 'morto'});
+
+
+        if(!loggedDev.nome == targetDev.likes){
+            targetDev.likes.push(loggedDev.nome);
+
+            targetDev.save();//salva dentro do array.
         }
-
-        targetDev.likes.push(loggedDev.nome);//é aqui onde adicionamos no array, troquei para salvar no destinatario.
-                                            //será que posso trocar o _id por number?
-
-        await targetDev.save();//salva dentro do array.
         
         return res.json(loggedDev);//retorna para o usuario logado.
     }
