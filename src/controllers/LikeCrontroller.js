c2e753b52c14c603765b07d237f4d0706ebfa7ff
SerginhoLeal <48488987+SerginhoLeal@users.noreply.github.com>
2020-05-02 00:27:49 -0300

    api-success

--- src/controllers/LikeCrontroller.js
+++ src/controllers/LikeCrontroller.js
@@ -9,17 +9,18 @@ module.exports = {
         const { devId } = req.params;//pega o usuario selecionado e não logado.
 
         const UsuarioLogado = await Grap.findById(user);//não preciso mexer em nada aqui.
-        const UsuarioReceptor = await Grap2.findById(devId);//eu poderia trocar o findById por create.
+        const UsuarioReceptor = await Grap2.findById(devId);//não preciso mexer em nada aqui.
+
+        const filterRocket = UsuarioReceptor.likes.filter(likes => likes.includes(UsuarioLogado.nome));
 
         try{
-            if(UsuarioLogado.nome == UsuarioReceptor.nome)//para que o criador não possa dar like em si mesmo
-                return res.status(400).json({error: 'Você não pode se avaliar'})
+            if(UsuarioLogado.nome == UsuarioReceptor.nome)//para que o criador não possa dar like em si mesmo.
+                return res.status(400).json({error: 'Você não pode se avaliar'})//retorna o aviso.
 
-            if(UsuarioLogado.nome == UsuarioReceptor.likes)//para que o criador não possa dar like em si mesmo
-                return res.status(400).json({error: 'Você Já deixou o seu like'})
+            if(UsuarioLogado.nome == filterRocket)//para que o usuário não possa dar like duas vezes.
+                return res.status(400).json({error: 'Você Já deixou o seu like'})//retorna o aviso.
 
-
-            UsuarioReceptor.likes.push(UsuarioLogado.nome);
+            UsuarioReceptor.likes.push(UsuarioLogado.nome);//Adicionando o nome do usuario logado no usuário receptor.
 
             await UsuarioReceptor.save();//salva dentro do array.
 
@@ -27,7 +28,7 @@ module.exports = {
             return res.status(400).send({error:'fail'});
         }
 
-        return res.json(UsuarioLogado);//retorna para o usuario logado.
+        return res.json(UsuarioReceptor);//retorna para o usuário logado.
     }
 };
 
